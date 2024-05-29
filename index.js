bccb269832b8e3c0536dc238282eebd843b0f49e
AYSTRONGIRP <ayirpstrong@gmail.com>
2024-05-30 00:15:33 +0530

    t

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const link = "https://lysos-assignmennt-backend.onrender.com"
+const link = "https://render-spinoff-bypass-2.onrender.com"
 console.log("starting from render 1");
 setInterval(() => {
     fetch(link).then(() =>{console.log(link);});
 }, 1000*60*10); //every 10 minutes
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
