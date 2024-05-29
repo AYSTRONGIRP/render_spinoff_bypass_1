ec7ff08aa9b26f3e5d82464422abf3063cab1a7a
AYSTRONGIRP <ayirpstrong@gmail.com>
2024-05-30 00:24:08 +0530

    f

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const link = "https://render-spinoff-bypass-2.onrender.com"
 console.log("starting from render 1");
 setInterval(() => {
-    fetch(link).then(() =>{console.log(link);});
-}, 1000*60*10); //every 10 minutes
+    fetch(link)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    console.log(response.data)
+    console.log(response)
+  })
+}, 1000); //every 1 minutes
 
 const express = require('express')
 const app = express()
